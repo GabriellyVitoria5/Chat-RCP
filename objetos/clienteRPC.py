8da47189987acd6097ed17ebced3244576c0f447
GabriellyVitoria <assuncaogabrielly5@gmail.com>
2024-11-06 18:04:17 -0300

    validando caminho do arquivo + comentários

--- objetos/clienteRPC.py
+++ objetos/clienteRPC.py
@@ -1,8 +1,22 @@
 import rpyc
+import os  
 
-proxy = rpyc.connect('localhost', 18861, config={'allow_public_attrs': True})
+caminho_arquivo = "arquivo_teste.txt"
 
-objeto_arquivo = open('arquivo_teste.txt')
+if os.path.exists(caminho_arquivo):
+    # se o arquivo existir, abre ele e conecta ao servidor
+    objeto_arquivo = open(caminho_arquivo)
 
-n_linhas = proxy.root.contador_linha(objeto_arquivo)
-print('Numero de linhas no arquivo: ', n_linhas)
+    # estabelece conexão com o servidor RPyC
+    proxy = rpyc.connect('localhost', 18861, config={'allow_public_attrs': True})
+    print("Conexão estabelecida com o servidor")
+
+    # faz a chamada remota ao servidor para contar as linhas no arquivo e depois exibir o resultado
+    n_linhas = proxy.root.contador_linha(objeto_arquivo)
+    print("Numero de linhas no arquivo: ", n_linhas)
+    
+    # fecha o arquivo depois do uso
+    objeto_arquivo.close()
+else:
+    # se o arquivo não existir, exibe uma mensagem de erro
+    print(f"O arquivo {caminho_arquivo} não foi encontrado.")
--- objetos/servidorRPC.py
+++ objetos/servidorRPC.py
@@ -1,10 +1,14 @@
 import rpyc
+from rpyc.utils.server import ThreadedServer
 
+# classe que permite ctiar serviços para ser chamados remotamente por clientes
 class MeuServico(rpyc.Service):
+    
+    # contar númetos de linhas de um arquivo txt
     def exposed_contador_linha(self, objeto_arquivo):
         n_linhas = len(objeto_arquivo.readlines())
         return n_linhas
 
-from rpyc.utils.server import ThreadedServer
+# criar e iniciar instância do servidor chamando a classe MeuServico
 t = ThreadedServer(MeuServico, port=18861)
 t.start()
